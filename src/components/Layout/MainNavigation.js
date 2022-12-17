342b6fba0bb87a387842afac109842c11aad0521
LordCracken <vladislav070201@gmail.com>
2022-12-17 04:09:31 +0300

    add logout functional

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -9,6 +9,10 @@ const MainNavigation = () => {
 
   const { isLoggedIn } = authCtx;
 
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -22,7 +26,7 @@ const MainNavigation = () => {
                 <Link to="/profile">Profile</Link>
               </li>
               <li>
-                <button>Logout</button>
+                <button onClick={logoutHandler}>Logout</button>
               </li>
             </>
           ) : (
